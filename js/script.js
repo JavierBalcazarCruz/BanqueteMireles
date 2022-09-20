8a53d5ea5c7a8bbaf8b16a2c9e2eec3cfe5f65a6
Javier Balcazar Cruz <javierbalcazarcruz@gmail.com>
2022-09-19 20:33:25 -0500

    Se termino el menu para tablet y movil, a si como correccciones visuales

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
-function CerrarMenu(){
-   document.getElementById('menu-btn');
+document.getElementById('menu-btn').onclick = function(){
+   var element = document.getElementById("ul-menu");
+   element.classList.add("active");
+}
+
+
+document.getElementById('menu-close').onclick = function(){
+   var element = document.getElementById("ul-menu");
+   element.classList.remove("active");
 }
