82a540dfe31aff2e60a433b76bbdeb5f35cf2a85
Javier Balcazar Cruz <javierbalcazarcruz@gmail.com>
2022-09-19 20:43:11 -0500

    mejoras en iconos de servicios

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
+/*Cuando da click al menu se activa*/
 document.getElementById('menu-btn').onclick = function(){
    var element = document.getElementById("ul-menu");
    element.classList.add("active");
 }
 
-
+/*Cuando da click al menu se desactiva*/
 document.getElementById('menu-close').onclick = function(){
    var element = document.getElementById("ul-menu");
    element.classList.remove("active");
